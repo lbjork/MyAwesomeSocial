5f0418fd28256e3cd1fcfae7e575484bc6409b28
Lars Björk <larsbjork@Larss-MacBook-Pro.local>
2016-11-14 19:21:40 +0100

    Completed setting up Firebase integration

---
+++ MyAwesomeSocial/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyAwesomeSocial
+//
+//  Created by Lars Björk on 2016-11-14.
+//  Copyright © 2016 Lars Björk. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
