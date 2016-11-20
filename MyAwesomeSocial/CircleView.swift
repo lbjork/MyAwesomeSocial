2963d351682b5f04d89edd061614901046cc2995
Lars Björk <lebjork@gmail.com>
2016-11-20 22:37:14 +0100

    Feed UI part 1 complete

--- MyAwesomeSocial/CircleView.swift
+++ MyAwesomeSocial/CircleView.swift
@@ -1,24 +1,29 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  MyAwesomeSocial
 //
-//  Created by Lars Björk on 2016-11-15.
+//  Created by Lars Björk on 2016-11-20.
 //  Copyright © 2016 Lars Björk. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
+class CircleView: UIImageView {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
-        
+
     }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        layer.cornerRadius = self.frame.width / 2
+    }
+
 
 }
