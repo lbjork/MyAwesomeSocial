e0fae577f5193e7045a3fda6bcdaae24524766ad
Lars Björk <lebjork@gmail.com>
2016-11-24 19:59:05 +0100

    Completed firebase display

--- MyAwesomeSocial/FeedVC.swift
+++ MyAwesomeSocial/FeedVC.swift
@@ -53,9 +53,14 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
         let post = posts[indexPath.row]
-        print("LARS: \(post.caption)")
-        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell {
+            cell.configureCell(post: post)
+            return cell
+        } else {
+            return PostCell()
+        }
     }
 
     @IBAction func signOutTapped(_ sender: UIButton) {
